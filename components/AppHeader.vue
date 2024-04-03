17e077e586feb580e28c40b7e336f73603789b17
Rabbit937 <hackrabbit@qq.com>
2024-04-03 18:39:54 +0800

    feat: 基本demo示例

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,3 +1,3 @@
 <template>
-  <NuxtPage />
+    <h1>Header</h1>
 </template>
---
+++ server/plugins/extend-html.ts
@@ -0,0 +1,11 @@
+export default defineNitroPlugin((nitroApp) => {
+    nitroApp.hooks.hook('render:html', (html, { event }) => {
+        console.log(html)
+
+        html.head.push(`<meta name="description" content="My custom description" />`)
+    })
+
+    nitroApp.hooks.hook('render:response', (response, { event }) => {
+        console.log(response)
+    })
+})
