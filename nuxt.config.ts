dc5b8d9425cc45cdd3b91fd79139627d47649305
Rabbit937 <hackrabbit@qq.com>
2024-05-07 11:42:53 +0800

    feat: 使用本地js

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      script: [
+        { src: "https://cdn.jsdelivr.net/npm/vue@3.4.27/dist/vue.global.min.js", type: 'text/javascript' },
+        { src: '/js/index.js', type: "text/javascript" },
+      ]
+    }
+  },
 })
